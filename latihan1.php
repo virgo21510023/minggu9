be90fb9ea4745793cf443c889e55e4a1b60d6ba1
virgo21510023 <virgo_21510023@stimata.ac.id>
2022-02-24 21:02:39 +0700

    menampilkan judul buku

--- latihan1.php
+++ latihan1.php
@@ -1,8 +1,47 @@
 <?php
 
-$_GET["nama"] = "Virgo";
-$_GET["NIM"] = "21510023";
-
-var_dump($_GET);
+$books = [
+	["title" => "Fugitive Pieces", 
+	"authors" => "Anne Michaels", 
+	"year" => "1996", 
+	"gambar" => "buku1.jpg"],
+	
+	["title" => "The Crown Conspiracy (The Riyria Revelations, #1)", 
+	"authors" => "Michael J. Sullivan", 
+	"year" => "2008", 
+	"gambar" => "buku2.jpg"],
+	
+	["title" => "Waking Up Married (Waking Up, #1)", 
+	"authors" => "Mira Lyn Kelly", 
+	"year" => "2012", 
+	"gambar" => "buku3.jpg"],
+	
+	["title" => "The Reckoning (Darkest Powers, #3)", 
+	"authors" => "Kelley Armstrong", 
+	"year" => "2010", 
+	"gambar" => "buku4.jpg"],
+	
+	["title" => "Angels in America", 
+	"authors" => "Tony Kushner", 
+	"year" => "1993", 
+	"gambar" => "buku5.jpg"],
+	
+];
 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Latihan GET</title>
+</head>
+<body>
+<h3>Daftar Buku</h3>
+<?php foreach ($books as $book): ?>
+<ul>
+	<li><?= $book["title"]; ?></li>
+</ul>
+<?php endforeach; ?>
+</body>
+</html>
